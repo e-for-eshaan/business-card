24ec3f7caf3957640076914f2ef546a2d8b6a8c2
e-for-eshaan <eshaanyadav24@gmail.com>
2023-07-29 14:52:50 +0530

    added qur

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 import "./Image.scss";
-
+import qr from "../../assets/qr.svg"
 
 import me from "../../assets/myImage.jpeg"
 
 export const Image = () => {
   return (
     <div className="imageSection">
-      <img className="profile" src={me} alt="" />
+      <div className="profile">
+        <img className="qrcode" src={qr} alt="" />
+        <img className="dp" src={me} alt="" />
+        <img src="" alt="" />
+      </div>
       <div>
         <h2>Eshaan Yadav</h2>
         <h3>Software Developer</h3>
